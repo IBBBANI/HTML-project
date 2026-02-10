9b9cf7adacd3b6026c3f145c8911b926069eb572
ibbani <ibbanigowda98@gmail.com>
2026-02-10 12:41:43 +0530

    Add JavaScript functionality for movie info

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Avengers: Endgame was released in 2019 and became one of the highest-grossing movies of all time!";
+}
